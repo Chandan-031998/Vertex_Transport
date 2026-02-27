d018fb3c77043c0ac632d6ca5c318f3540fba06a
Chandan10 <chandangirish95@gmail.com>
2026-02-27 18:30:49 +0530

    Updated project

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
-// if Root Directory = server, then file path becomes: server/api/index.js
-import app from "./src/app.js";
+// server/api/index.js
+import app from "../src/app.js";
 
 export default function handler(req, res) {
   return app(req, res);
